e0e0c642374b825f3740a023111ba209666f8eda
Tomás Fioroni <tomasfioroni@gmail.com>
2022-06-19 04:10:22 -0300

    Dejo más completo el ejercicio 6

--- src/ejercicio6.py
+++ src/ejercicio6.py
@@ -4,7 +4,6 @@
 ################
 """
 Implementar una funcion que codifique un texto rotandolo una cantidad de posiciones ajustable.
-
 Implementar la funcion que decodifique el texto rotado una cantidad de posiciones ajustable.
 """
 def encriptador(texto, cantidad):
@@ -54,7 +53,55 @@ def encriptador(texto, cantidad):
     return unicode
 
 
-def desencriptador(texto, unicode):
+def desencriptador(texto_encriptado, cantidad):
+    """
+    Función que desencripta el mensaje en encriptado como str por el usuario
+    """
+    cantidad_original = cantidad
+    limite = len(texto_encriptado)
+    i = 0
+    desencriptado = []
+    while limite > 0:
+        caracter = ord(texto_encriptado[i])
+        if texto_encriptado[i].isupper():
+            maximo = 90
+            while caracter >= 65 and cantidad > 0:
+                caracter -= 1
+                cantidad -= 1
+                if caracter < 65:
+                    caracter = maximo
+                else:
+                    continue
+        elif texto_encriptado[i].islower():
+            maximo = 122
+            while caracter >= 97 and cantidad > 0:
+                caracter -= 1
+                cantidad -= 1
+                if caracter < 97:
+                    caracter = maximo
+                else:
+                    continue
+        elif texto_encriptado[i].isdigit():
+            maximo = 57
+            while caracter >= 48 and cantidad > 0:
+                caracter -= 1
+                cantidad -= 1
+                if caracter < 48:
+                    caracter = maximo
+                else:
+                    continue
+        else:
+            pass
+        limite -= 1
+        i += 1
+        cantidad = cantidad_original
+        desencriptado.append(caracter)
+    return desencriptado
+
+
+
+
+def traductor(texto, unicode):
     """
     Función que desencripta el texto encriptado por el usuario
     """
@@ -67,7 +114,7 @@ def desencriptador(texto, unicode):
         encriptado.append(caracter)
         limite -= 1
         i += 1
-    print(salida.join(encriptado))
+    salida = salida.join(encriptado)
     return salida
 
 
@@ -80,7 +127,12 @@ def principal():
     cantidad = int(input("Introduzca una cantidad de carácteres para cifrar: "))
     unicode = encriptador(texto, cantidad)
     print(unicode)
-    print(desencriptador(texto, unicode))
+    encriptado = traductor(texto, unicode)
+    print(encriptado)
+    unicode_desencriptado = desencriptador(encriptado, cantidad)
+    print(unicode_desencriptado)
+    final = traductor(texto, unicode_desencriptado)
+    print(final)
 if __name__ == "__main__":
     principal()
 
